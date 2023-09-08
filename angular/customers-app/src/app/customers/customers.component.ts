ae5ad76f4c7794db940f995f4fb428e12b990674
Carlos Oliva <carlos.oliva@eviden.com>
2023-09-08 12:24:10 +0200

    Creando archivo clientes.json.ts con la lista de objetos

--- angular/customers-app/src/app/customers/customers.component.ts
+++ angular/customers-app/src/app/customers/customers.component.ts
@@ -1,21 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Customer } from './customer';
+import { CUSTOMERS } from './customers.json';
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html'
 })
-export class CustomersComponent {
+export class CustomersComponent  implements OnInit {
 
-  customers: Customer[] = [
-    {id: 1, name: 'Carlos', surname: 'Oliva', createdAt: '2023-09-08', email: 'colidom@outlook.com'},
-    {id: 2, name: 'Javier', surname: 'Oliva', createdAt: '2023-09-08', email: 'colidom@outlook.com'},
-    {id: 3, name: 'Jhon', surname: 'Doe', createdAt: '2023-09-08', email: 'jhondoe@outlook.com'},
-    {id: 4, name: 'Fran', surname: 'Franco', createdAt: '2023-09-08', email: 'francisfranco@outlook.com'},
-    {id: 5, name: 'David', surname: 'Yanes', createdAt: '2023-09-08', email: 'davidyanes@outlook.com'},
-    {id: 6, name: 'Tim', surname: 'Berners-Lee', createdAt: '2023-09-08', email: 'timbernerslee@outlook.com'},
-  ];
+  customers: Customer[] | undefined;
 
   constructor() {
     
+  }
+
+  ngOnInit() {
+    this.customers = CUSTOMERS;
   }
 }
---
+++ angular/customers-app/src/app/customers/customers.json.ts
@@ -0,0 +1,10 @@
+import { Customer } from './customer';
+
+export const CUSTOMERS : Customer[] = [
+    {id: 1, name: 'Carlos', surname: 'Oliva', createdAt: '2023-09-08', email: 'colidom@outlook.com'},
+    {id: 2, name: 'Javier', surname: 'Oliva', createdAt: '2023-09-08', email: 'colidom@outlook.com'},
+    {id: 3, name: 'Jhon', surname: 'Doe', createdAt: '2023-09-08', email: 'jhondoe@outlook.com'},
+    {id: 4, name: 'Fran', surname: 'Franco', createdAt: '2023-09-08', email: 'francisfranco@outlook.com'},
+    {id: 5, name: 'David', surname: 'Yanes', createdAt: '2023-09-08', email: 'davidyanes@outlook.com'},
+    {id: 6, name: 'Tim', surname: 'Berners-Lee', createdAt: '2023-09-08', email: 'timbernerslee@outlook.com'},
+  ];
