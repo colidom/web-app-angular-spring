555e05b65ca6c139b2cd12b91e85501b362a96de
Carlos Oliva <colidom@outlook.com>
2023-09-11 12:19:50 +0200

    Escribiendo los métodos update y delete en el Controlador Backend API Rest

--- eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/CustomerRestController.java
+++ eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/CustomerRestController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -39,5 +41,22 @@ public class CustomerRestController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Customer create(@RequestBody Customer customer) {
 		return customerService.save(customer);
+	}
+	
+	@PutMapping("/customers/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Customer update(@RequestBody Customer customer, @PathVariable Long id) {
+		Customer currentCustomer = customerService.findById(id);
+		currentCustomer.setName(customer.getName());
+		currentCustomer.setSurname(customer.getSurname());
+		currentCustomer.setEmail(customer.getEmail());
+		
+		return customerService.save(currentCustomer);
+	}
+	
+	@DeleteMapping("/customers/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		customerService.delete(id);
 	}
 }
