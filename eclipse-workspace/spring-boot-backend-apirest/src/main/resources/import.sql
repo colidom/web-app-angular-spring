140a6f3fbb877f98ff5ed8cdbe7b304c9db9820e
Carlos Oliva <colidom@outlook.com>
2023-09-11 23:51:31 +0200

    Overflow en listado de clientes, ajustando layout

--- eclipse-workspace/spring-boot-backend-apirest/src/main/resources/import.sql
+++ eclipse-workspace/spring-boot-backend-apirest/src/main/resources/import.sql
@@ -13,3 +13,18 @@ INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Ma
 INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Boba', 'Fett', '31.5 BBY', '1948-03-28', 'boba@bountyhunter.com');
 INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('R2-D2', 'Droid', 'Before 32 BBY', '2023-03-03', 'r2d2@droid.com');
 INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('C-3PO', 'Droid', 'Before 32 BBY', '2023-10-06', 'c3po@droid.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Rey', 'Skywalker', 'Unknown', '2025-02-10', 'rey@jedi.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Finn', 'Stormtrooper', 'Unknown', '2025-03-15', 'finn@resistance.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Kylo', 'Ren', '5 ABY', '2019-12-20', 'kylo@firstorder.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Poe', 'Dameron', 'Unknown', '2024-07-05', 'poe@resistance.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Hera', 'Syndulla', 'Unknown', '2022-06-30', 'hera@rebels.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Qui-Gon', 'Jinn', 'Unknown', '1999-05-19', 'qui-gon.jinn@jedi.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Aayla', 'Secura', '48 BBY', '1977-05-25', 'aayla.secura@jedi.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Kit', 'Fisto', 'Unknown', '2005-03-15', 'kit.fisto@jedi.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Asajj', 'Ventress', 'Unknown', '2003-12-20', 'asajj.ventress@sith.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Grievous', 'Cyborg', 'Unknown', '2008-07-05', 'grievous@separatists.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Jango', 'Fett', 'Unknown', '2002-06-30', 'jango.fett@bountyhunter.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Aurra', 'Sing', 'Unknown', '2002-06-30', 'aurra.sing@bountyhunter.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Cad', 'Bane', 'Unknown', '2008-06-13', 'cad.bane@bountyhunter.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Hondo', 'Ohnaka', 'Unknown', '2008-06-13', 'hondo.ohnaka@pirate.com');
+INSERT INTO Customers (name, surname, birth_date, created_at, email) VALUES ('Emperor', 'Sith', 'Unknown', '1977-05-25', 'emperor@sithempire.com');
