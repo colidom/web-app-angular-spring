c994967af9c02f798964a486354cbbf8fcda00a0
Carlos Oliva <colidom@outlook.com>
2023-09-15 08:46:01 +0200

    Qquitado método innecesario

--- eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IUserDao.java
+++ eclipse-workspace/spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IUserDao.java
@@ -1,6 +1,5 @@
 package com.bolsadeideas.springboot.backend.apirest.models.dao;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entity.User;
@@ -8,8 +7,5 @@ import com.bolsadeideas.springboot.backend.apirest.models.entity.User;
 public interface IUserDao extends CrudRepository<User, Long> {
 
 	public User findByUsername(String username);
-
-	@Query("select u from Usuario u were u.username=?1")
-	public User findByUsername2(String username);
 
 }
